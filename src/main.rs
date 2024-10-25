78564be7c445608efe8e60d4631ca8d216a7abef
NAVYSHUNTA <jmsdfnavy.5491@gmail.com>
2024-10-26 00:08:36 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use cpu_time::ProcessTime; // https://crates.io/crates/cpu-time
+use std::time::Duration;
+use algo_comp_exp_code::*;
+
+pub enum Algorithm {
+    SR1,
+    SR2,
+    SR3,
+}
+
+fn main() {
+    const INPUT_MAX_X: i128 = 1000; // 実行時間が長い場合は 100 などに変更すると良い
+
+    // 以下のコメントアウトを外して、各アルゴリズムの実験結果を表示
+    print_experiment_result(INPUT_MAX_X, Algorithm::SR1);
+    print_experiment_result(INPUT_MAX_X, Algorithm::SR2);
+    print_experiment_result(INPUT_MAX_X, Algorithm::SR3);
+}
+
+pub fn print_experiment_result(max_x: i128, select_algorithm: Algorithm) {
+    let target_algorithm: fn(i128) -> i128 = match select_algorithm {
+        Algorithm::SR1 => sr1,
+        Algorithm::SR2 => sr2,
+        Algorithm::SR3 => sr3,
+    };
+
+    let mut datas: Vec<(i128, u128)> = Vec::new();
+    for x in 1i128..=max_x {
+        let start_time: ProcessTime = ProcessTime::now(); // 計測開始
+        let _res: i128 = target_algorithm(x);
+        let duration: Duration = start_time.elapsed(); // 計測終了（計測結果）
+        datas.push((x, duration.as_micros()));
+    }
+
+    let algorithm_name: &str = match select_algorithm {
+        Algorithm::SR1 => "SR1",
+        Algorithm::SR2 => "SR2",
+        Algorithm::SR3 => "SR3",
+    };
+
+    println!("{} の実験結果", algorithm_name);
+    for (x, time) in datas {
+        println!("{}, {}", x, time);
+    }
+}
---
+++ tests/lib_test.rs
@@ -0,0 +1,73 @@
+use algo_comp_exp_code::*;
+
+// アルゴリズム SR1 の単体テスト
+#[test]
+fn test_sr1_succeeds() {
+    assert_eq!(sr1(1i128), 1i128);
+    assert_eq!(sr1(2i128), 6i128);
+    assert_eq!(sr1(3i128), 18i128);
+
+    assert_ne!(sr1(1i128), 2i128);
+    assert_ne!(sr1(2i128), 3i128);
+    assert_ne!(sr1(3i128), 6i128);
+}
+
+#[test]
+#[should_panic(expected = "正の整数を入力してください.")]
+fn test_sr1_input_zero_panic() {
+    sr1(0i128);
+}
+
+#[test]
+#[should_panic(expected = "正の整数を入力してください.")]
+fn test_sr1_input_negative_one_panic() {
+    sr1(-1i128);
+}
+
+// アルゴリズム SR2 の単体テスト
+#[test]
+fn test_sr2_succeeds() {
+    assert_eq!(sr2(1i128), 1i128);
+    assert_eq!(sr2(2i128), 6i128);
+    assert_eq!(sr2(3i128), 18i128);
+
+    assert_ne!(sr2(1i128), 2i128);
+    assert_ne!(sr2(2i128), 3i128);
+    assert_ne!(sr2(3i128), 6i128);
+}
+
+#[test]
+#[should_panic(expected = "正の整数を入力してください.")]
+fn test_sr2_input_zero_panic() {
+    sr2(0i128);
+}
+
+#[test]
+#[should_panic(expected = "正の整数を入力してください.")]
+fn test_sr2_input_negative_one_panic() {
+    sr2(-1i128);
+}
+
+// アルゴリズム SR3 の単体テスト
+#[test]
+fn test_sr3_succeeds() {
+    assert_eq!(sr3(1i128), 1i128);
+    assert_eq!(sr3(2i128), 6i128);
+    assert_eq!(sr3(3i128), 18i128);
+
+    assert_ne!(sr3(1i128), 2i128);
+    assert_ne!(sr3(2i128), 3i128);
+    assert_ne!(sr3(3i128), 6i128);
+}
+
+#[test]
+#[should_panic(expected = "正の整数を入力してください.")]
+fn test_sr3_input_zero_panic() {
+    sr3(0i128);
+}
+
+#[test]
+#[should_panic(expected = "正の整数を入力してください.")]
+fn test_sr3_input_negative_one_panic() {
+    sr3(-1i128);
+}
