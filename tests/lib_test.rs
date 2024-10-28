4d8a07ccb7b1cbc95c0289164d945da5d1d44203
NAVYSHUNTA <jmsdfnavy.5491@gmail.com>
2024-10-29 06:26:03 +0900

    テストを強化した。

--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -7,9 +7,9 @@ fn test_sr1_succeeds() {
     assert_eq!(sr1(2i128), 6i128);
     assert_eq!(sr1(3i128), 18i128);
 
-    assert_ne!(sr1(1i128), 2i128);
-    assert_ne!(sr1(2i128), 3i128);
-    assert_ne!(sr1(3i128), 6i128);
+    assert_ne!(sr1(4i128), 39i128);
+    assert_ne!(sr1(5i128), 76128);
+    assert_ne!(sr1(6i128), 127i128);
 }
 
 #[test]
@@ -31,9 +31,9 @@ fn test_sr2_succeeds() {
     assert_eq!(sr2(2i128), 6i128);
     assert_eq!(sr2(3i128), 18i128);
 
-    assert_ne!(sr2(1i128), 2i128);
-    assert_ne!(sr2(2i128), 3i128);
-    assert_ne!(sr2(3i128), 6i128);
+    assert_ne!(sr2(4i128), 39i128);
+    assert_ne!(sr2(5i128), 76i128);
+    assert_ne!(sr2(6i128), 127i128);
 }
 
 #[test]
@@ -55,9 +55,9 @@ fn test_sr3_succeeds() {
     assert_eq!(sr3(2i128), 6i128);
     assert_eq!(sr3(3i128), 18i128);
 
-    assert_ne!(sr3(1i128), 2i128);
-    assert_ne!(sr3(2i128), 3i128);
-    assert_ne!(sr3(3i128), 6i128);
+    assert_ne!(sr3(4i128), 39i128);
+    assert_ne!(sr3(5i128), 76i128);
+    assert_ne!(sr3(6i128), 127i128);
 }
 
 #[test]
