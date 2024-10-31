337b6d8abc5e55a2040bfe7d57e685b7a1c7f98c
NAVYSHUNTA <jmsdfnavy.5491@gmail.com>
2024-10-31 15:41:04 +0900

    リファクタリング

--- graph.py
+++ graph.py
@@ -9,30 +9,27 @@ import japanize_matplotlib
 # フォントサイズを設定
 plt.rcParams['font.size'] = 15
 
-# SR1, SR2, SR3 の比較
-data1 = np.loadtxt('output1.txt', skiprows=2, delimiter=',')
-data2 = np.loadtxt('output2.txt', skiprows=2, delimiter=',')
-data3 = np.loadtxt('output3.txt', skiprows=2, delimiter=',')
+# 出力ファイル名とアルゴリズム名、線の色の対応
+get_algorithm_name_and_line_color = {
+    'output1.txt': ['SR1', 'red'],
+    'output2.txt': ['SR2', 'green'],
+    'output3.txt': ['SR3', 'blue'],
+    'output2_big.txt': ['SR2', 'green'],
+    'output3_big.txt': ['SR3', 'blue'],
+}
 
-plt.plot(data1[:,0], data1[:,1], label='SR1', color='red')
-plt.plot(data2[:,0], data2[:,1], label='SR2', color='green')
-plt.plot(data3[:,0], data3[:,1], label='SR3', color='blue')
+# SR1, SR2, SR3 のうち指定されたものを比較するための図を作成する関数
+def get_graph(*file_names):
+    for file_name in file_names:
+        data = np.loadtxt(file_name, skiprows=2, delimiter=',')
+        algorithm_name, line_color = get_algorithm_name_and_line_color[file_name]
+        plt.plot(data[:,0], data[:,1], label=algorithm_name, color=line_color)
 
-plt.xlabel('x の値') # x 軸のラベル
-plt.ylabel('処理時間 [μs]') # y 軸のラベル
-plt.legend() # 凡例を表示
-plt.tight_layout()
-plt.show()
+    plt.xlabel('x の値') # x 軸のラベル
+    plt.ylabel('処理時間 [μs]') # y 軸のラベル
+    plt.legend() # 凡例を表示
+    plt.tight_layout()
+    plt.show()
 
-# SR2, SR3 の比較
-bigdata2 = np.loadtxt('output2_big.txt', skiprows=2, delimiter=',')
-bigdata3 = np.loadtxt('output3_big.txt', skiprows=2, delimiter=',')
-
-plt.plot(bigdata2[:,0], bigdata2[:,1], label='SR2', color='green')
-plt.plot(bigdata3[:,0], bigdata3[:,1], label='SR3', color='blue')
-
-plt.xlabel('x の値') # x 軸のラベル
-plt.ylabel('処理時間 [μs]') # y 軸のラベル
-plt.legend() # 凡例を表示
-plt.tight_layout()
-plt.show()
+get_graph('output1.txt', 'output2.txt', 'output3.txt')
+get_graph('output2_big.txt', 'output3_big.txt')
