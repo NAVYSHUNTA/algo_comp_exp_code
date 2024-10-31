7b93602e3fb0b92dc52eb740061324bd14cf32ed
NAVYSHUNTA <jmsdfnavy.5491@gmail.com>
2024-11-01 04:19:50 +0900

    リファクタリング

--- graph.py
+++ graph.py
@@ -14,15 +14,13 @@ get_algorithm_name_and_line_color = {
     'output1.txt': ['SR1', 'red'],
     'output2.txt': ['SR2', 'green'],
     'output3.txt': ['SR3', 'blue'],
-    'output2_big.txt': ['SR2', 'green'],
-    'output3_big.txt': ['SR3', 'blue'],
 }
 
 # SR1, SR2, SR3 のうち指定されたものを比較するための図を作成する関数
 def get_graph(*file_names):
     for file_name in file_names:
         data = np.loadtxt(file_name, skiprows=2, delimiter=',')
-        algorithm_name, line_color = get_algorithm_name_and_line_color[file_name]
+        algorithm_name, line_color = get_algorithm_name_and_line_color[file_name.replace('_big', '')]
         plt.plot(data[:,0], data[:,1], label=algorithm_name, color=line_color)
 
     plt.xlabel('x の値') # x 軸のラベル
