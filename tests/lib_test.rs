8266397a0976383b2262332954d1b8d2473c6048
NAVYSHUNTA <jmsdfnavy.5491@gmail.com>
2024-11-28 15:29:43 +0900

    リファクタリング

--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -3,13 +3,13 @@ use algo_comp_exp_code1::*;
 // アルゴリズム SR1 の単体テスト
 #[test]
 fn test_sr1_succeeds() {
-    assert_eq!(sr1(1i128), 1i128);
-    assert_eq!(sr1(2i128), 6i128);
-    assert_eq!(sr1(3i128), 18i128);
+    assert_eq!(1i128, sr1(1i128));
+    assert_eq!(6i128, sr1(2i128));
+    assert_eq!(18i128, sr1(3i128));
 
-    assert_ne!(sr1(4i128), 39i128);
-    assert_ne!(sr1(5i128), 76128);
-    assert_ne!(sr1(6i128), 127i128);
+    assert_ne!(39i128, sr1(4i128));
+    assert_ne!(76i128, sr1(5i128));
+    assert_ne!(127i128, sr1(6i128));
 }
 
 #[test]
@@ -27,13 +27,13 @@ fn test_sr1_input_negative_one_panic() {
 // アルゴリズム SR2 の単体テスト
 #[test]
 fn test_sr2_succeeds() {
-    assert_eq!(sr2(1i128), 1i128);
-    assert_eq!(sr2(2i128), 6i128);
-    assert_eq!(sr2(3i128), 18i128);
+    assert_eq!(1i128, sr2(1i128));
+    assert_eq!(6i128, sr2(2i128));
+    assert_eq!(18i128, sr2(3i128));
 
-    assert_ne!(sr2(4i128), 39i128);
-    assert_ne!(sr2(5i128), 76i128);
-    assert_ne!(sr2(6i128), 127i128);
+    assert_ne!(39i128, sr2(4i128));
+    assert_ne!(76i128, sr2(5i128));
+    assert_ne!(127i128, sr2(6i128));
 }
 
 #[test]
@@ -51,13 +51,13 @@ fn test_sr2_input_negative_one_panic() {
 // アルゴリズム SR3 の単体テスト
 #[test]
 fn test_sr3_succeeds() {
-    assert_eq!(sr3(1i128), 1i128);
-    assert_eq!(sr3(2i128), 6i128);
-    assert_eq!(sr3(3i128), 18i128);
+    assert_eq!(1i128, sr3(1i128));
+    assert_eq!(6i128, sr3(2i128));
+    assert_eq!(18i128, sr3(3i128));
 
-    assert_ne!(sr3(4i128), 39i128);
-    assert_ne!(sr3(5i128), 76i128);
-    assert_ne!(sr3(6i128), 127i128);
+    assert_ne!(39i128, sr3(4i128));
+    assert_ne!(76i128, sr3(5i128));
+    assert_ne!(127i128, sr3(6i128));
 }
 
 #[test]
